6117901e12b44db4f1134895c032cc2c59163c79
sync65534 <142959904+sync65534@users.noreply.github.com>
2025-06-21 22:44:10 -0400

    Update winsockwrap.h
    added ability to select logging and error options

--- winsockwrap.h
+++ winsockwrap.h
@@ -1,80 +1,96 @@
 #ifndef WINSOCKWRAP
 #define WINSOCKWRAP
-
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <optional>
 class sckman {
+    private:
+        inline void throw_err(const char* context, int sock_fd, int WSAErrCode){
+            fprintf(stderr, "%s failure on socket %d\nWSA Error code: %d\n", context, sock_fd, WSAErrCode);
+        }
     public:
-        int sck_fd;
-        sockaddr* addr;
-        int addrlen;
+        int socket_fd;
+        sockaddr_in addr;
+        int addrlen = sizeof(addr);
+        bool fatal_on_error = false;    // Setting default for entire object
+        bool log_on_error = true;          // Setting default for entire object
 
 
-        sckman(){
-            addrlen = sizeof(addr);
-        }
-        int Ssocket(int af, int type, int protocol){
-            sck_fd = socket(af, type, protocol);
-            if (sck_fd == INVALID_SOCKET){
-                WSACleanup();
-                return WSAGetLastError();
+        int Ssocket(int af, int type, int protocol, std::optional<bool> fatal = std::nullopt, std::optional<bool> log = std::nullopt){
+            socket_fd = socket(af, type, protocol);
+            if (socket_fd == INVALID_SOCKET){
+                if (fatal.value_or(fatal_on_error)) { WSACleanup(); }
+                int err_code =  WSAGetLastError();
+                if (log.value_or(log_on_error)) {throw_err("Socket", socket_fd, err_code);}
+                return err_code;
             }
             return 0;
         }
-
-        int Sbind(){    // Fills addr and addrlen from the objects addr
-            if (bind(sck_fd, addr, addrlen) == SOCKET_ERROR){
-                WSACleanup();
-                closesocket(sck_fd);
-                return WSAGetLastError();
+        int Sbind(std::optional<bool> fatal = std::nullopt, std::optional<bool> log = std::nullopt){    // Fills addr and addrlen from the objects addr
+            if (bind(socket_fd, (sockaddr*)&addr, addrlen) == SOCKET_ERROR){
+                if (fatal.value_or(fatal_on_error)) {WSACleanup();}
+                int err_code = WSAGetLastError();
+                if (log.value_or(log_on_error)) {throw_err("Bind", socket_fd, err_code);}
+                closesocket(socket_fd);
+                return err_code;
             }
             return 0;
         }
-        int Slisten(int backlog){
-            if (listen(sck_fd, backlog) == SOCKET_ERROR){
-                WSACleanup();
-                closesocket(sck_fd);
-                return WSAGetLastError();
+        int Slisten(int backlog, std::optional<bool> fatal = std::nullopt, std::optional<bool> log = std::nullopt){
+            if (listen(socket_fd, backlog) == SOCKET_ERROR){
+                if (fatal.value_or(fatal_on_error)) {WSACleanup();};
+                int err_code = WSAGetLastError();
+                closesocket(socket_fd);
+                if (log.value_or(log_on_error)) {throw_err("Listen", socket_fd, err_code);}
+                return err_code;
             }
             return 0;
         }
-        SOCKET Saccept(sockaddr* clientAddr, int* clientAddrLen){
-            SOCKET value_buf = accept(sck_fd, clientAddr, clientAddrLen);
+        int Srecv(char* buf, int len, int flags, std::optional<bool> fatal = std::nullopt, std::optional<bool> log = std::nullopt){
+            int value_buf = recv(socket_fd, buf, len, flags);
+            if (value_buf == SOCKET_ERROR){
+                if (fatal.value_or(fatal_on_error)) {WSACleanup();};
+                    int err_code = WSAGetLastError();
+                    closesocket(socket_fd);
+                    if (log.value_or(log_on_error)) {throw_err("Recv", socket_fd, err_code);}
+                    return err_code;
+                }
+            return value_buf;
+        }
+        int Ssend(char* buf, int len, int flags, std::optional<bool> fatal = std::nullopt, std::optional<bool> log = std::nullopt){
+            int value_buf = send(socket_fd, buf, len, flags); 
+            if (value_buf == SOCKET_ERROR) {
+                int err_code = WSAGetLastError();
+                if (fatal.value_or(fatal_on_error)) { WSACleanup(); }
+                if (log.value_or(log_on_error)) { throw_err("Send", socket_fd, err_code); }
+                closesocket(socket_fd);
+                return err_code;
+            }
+            return value_buf;
+        }
+        int Sshutdown(int how, std::optional<bool> fatal = std::nullopt, std::optional<bool> log = std::nullopt){
+            if (shutdown(socket_fd, how) == SOCKET_ERROR){
+                int err_code = WSAGetLastError();
+                if (fatal.value_or(fatal_on_error)) {WSACleanup();};
+                if (log.value_or(log_on_error)) {throw_err("Shutdown", socket_fd, err_code);}
+                closesocket(socket_fd);
+                return err_code;
+            }
+            return 0;
+        }
+        SOCKET Saccept(sockaddr* clientAddr, int* clientAddrLen, std::optional<bool> fatal = std::nullopt, std::optional<bool> log = std::nullopt){
+            SOCKET value_buf = accept(socket_fd, clientAddr, clientAddrLen);
             if (value_buf == INVALID_SOCKET){
-                WSACleanup();
-                closesocket(sck_fd);
-                return WSAGetLastError();
+                if (fatal.value_or(fatal_on_error)) {WSACleanup();};
+                int err_code = WSAGetLastError();
+                closesocket(socket_fd);
+                if (log.value_or(log_on_error)) {throw_err("Accept", socket_fd, err_code);}
+                return err_code;
             }
             return value_buf;
         }
-        int Srecv(char* buf, int len, int flags){
-            int value_buf = recv(sck_fd, buf, len, flags);
-            if (value_buf == SOCKET_ERROR){
-                WSACleanup();
-                closesocket(sck_fd);
-                return WSAGetLastError();
-            }
-            return value_buf;
-        }
-        int Ssend(char* buf, int len, int flags){
-            int value_buf = send(sck_fd, buf, len, flags); 
-            if (value_buf == SOCKET_ERROR){
-                WSACleanup();
-                closesocket(sck_fd);
-                return WSAGetLastError();
-            }
-            return value_buf;
-        }
-        int Sshutdown(int how){
-            if (shutdown(sck_fd, how) == SOCKET_ERROR){
-                WSACleanup();
-                closesocket(sck_fd);
-            }
-            return 0;
-        }
-
+        
 };
 #endif
